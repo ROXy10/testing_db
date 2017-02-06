1f9a022a3afa7d5f09385ed851d4ad08fb5ddd80
ROXy <djeffik@gmail.com>
2017-02-06 15:20:38 +0200

    admin: add admin view

--- backend/testing/admin.py
+++ backend/testing/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
+
 from .models import Testing, Test, Question, Answer
+
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    show_change_link = True
+
+
+class AnswerInline(admin.TabularInline):
+    model = Answer
+    show_change_link = True
 
 
 class TestAdmin(admin.ModelAdmin):
@@ -12,8 +23,28 @@ class TestAdmin(admin.ModelAdmin):
     list_per_page = 10
     search_fields = ['test_title']
 
+    inlines = [
+        QuestionInline,
+    ]
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['question_title', 'test', 'multi']
+    list_display_links = ['question_title']
+    list_editable = ['test', 'multi']
+    ordering = ['test', 'question_title']
+    list_filter = ['test', 'multi']
+
+    list_per_page = 10
+    search_fields = ['question_title']
+
+    inlines = [
+        AnswerInline,
+    ]
+
+# date_hierarchy = 'date'
 
 admin.site.register(Testing)
 admin.site.register(Test, TestAdmin)
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Answer)
